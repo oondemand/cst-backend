83f168c557df055179d34c226e87701cb0c310e7
Maike <maikonalexandre574@gmail.com>
2025-04-24 07:35:04 -0300

    refactor: rename service and update configurations for esteira-de-servicos-backend

--- src/controllers/aprovacaoController.js
+++ src/controllers/aprovacaoController.js
@@ -349,11 +349,9 @@ const cadastrarContaAPagar = async (baseOmie, codigoFornecedor, ticket) => {
       dataVencimento: add(dataDaEmissão, { hours: 24 }), // 24 horas a mais
       observacao,
       valor: valorTotalDaNota,
-      id_conta_corrente:
-        config?.omie?.id_conta_corrente ?? process.env.ID_CONTA_CORRENTE,
+      id_conta_corrente: config?.omie?.id_conta_corrente,
       dataRegistro: ticket?.servicos[0]?.dataRegistro,
-      codigo_categoria:
-        config?.omie?.codigo_categoria ?? process.env.CODIGO_CATEGORIA,
+      codigo_categoria: config?.omie?.codigo_categoria,
     });
 
     const contaPagarOmie = await contaPagarService.incluir(
